c8fccfbf24940810853a738fe517b616f7771c8f
brendobrendo <brendan.smith903@gmail.com>
2022-02-20 19:55:48 -0800

    Link updates on homepage

--- flask_app/controllers/routes_emails.py
+++ flask_app/controllers/routes_emails.py
@@ -1,7 +1,9 @@
 from flask_app import app
-from flask_app.models import model_members, model_officers
+from flask_app.models import model_members, model_officers, model_role_assignments
 from flask import redirect, render_template, request, session
 import smtplib
+from email.message import EmailMessage
+
 
 @app.route("/new_email")
 def new_email():
@@ -10,13 +12,35 @@ def new_email():
     user = model_members.Member.get_one({'id': session['user_id']})
     return render_template("toastmaster_email.html", user=user)
 
+
 @app.route("/send_email", methods=['POST'])
 def send_email():
     data = request.form
     subject = data.get("subject")
     body = data.get("body")
 
-    msg = f'Subject: {subject}\n\n{body}'
+    msg = EmailMessage()
+    msg['Subject'] = subject
+    msg['From'] = "tot4741.toastmaster@gmail.com"
+    msg['To'] = "brendan.smith903@gmail.com"
+    msg.set_content(body)
+
+    upcoming_roles = model_role_assignments.RoleAssignment.get_upcoming_user(
+        {'member_id': session['user_id']})
+    upcoming_role = upcoming_roles[0].role_name
+
+    msg.add_alternative("""
+    <!DOCTYPE html>
+    <html>
+        <body>
+            <h1 style="color:blue;">
+    """ +
+                        f'{body} </h1>' +
+            "<p>Upcoming Role: " + f'{upcoming_role} </p>' +
+                        """
+        </body>
+    </html>
+    """, subtype='html')
 
     server = smtplib.SMTP("smtp.gmail.com", 587)
     server.starttls()
@@ -28,8 +52,7 @@ def send_email():
         server.sendmail("tot4741.toastmaster@gmail.com", officer.email, content)
     """
 
-    server.sendmail("tot4741.toastmaster@gmail.com", "brendan.smith903@gmail.com", msg)
-
+    server.send_message(msg)
 
     server.quit()
     return redirect("/officers_page")
--- flask_app/models/model_officers.py
+++ flask_app/models/model_officers.py
@@ -1,8 +1,9 @@
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask_app import DATABASE
 
+
 class Officer:
-    def __init__( self , data ):
+    def __init__(self, data):
         self.id = data['id']
         self.officer_first_name = data['first_name']
         self.officer_last_name = data['last_name']
@@ -10,21 +11,21 @@ class Officer:
         self.title_name = data['officer_title']
         self.title_id = data['officer_role_names.id']
         self.email = data['email']
-    
+
     # C - Create methods / INSERT a new entry into a table
     @classmethod
     def save(cls, data):
         query = "INSERT INTO officer_assignments (member_id, officer_role_name_id) VALUES (%(member_id)s, %(officer_role_name_id)s);"
         return connectToMySQL(DATABASE).query_db(query, data)
-    
+
     # R - Read methods / return data from table
     @classmethod
     def get_all(cls):
         query = "SELECT officer_assignments.id, members.first_name, members.last_name, members.id, members.email, officer_role_names.officer_title, officer_role_names.id FROM officer_assignments JOIN officer_role_names ON officer_assignments.officer_role_name_id = officer_role_names.id JOIN members ON officer_assignments.member_id = members.id;"
         results = connectToMySQL(DATABASE).query_db(query)
-        officers = [] 
+        officers = []
         for officer in results:
-            officers.append( cls(officer) )
+            officers.append(cls(officer))
         return officers  # Returns a list of class instances
 
     @staticmethod
@@ -33,7 +34,6 @@ class Officer:
         for officer in data:
             officer_ids.append(officer['officer_member_id'])
         return officer_ids
-
 
     # U - Update methods / UPDATE existing entries with new values
 
